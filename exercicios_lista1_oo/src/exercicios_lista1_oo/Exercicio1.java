697038ebf95a4b03b556edc624beb9760d270a99
isaqzin <isaquereserva@gmail.com>
2024-03-18 21:03:04 -0300

    testando

---
+++ exercicios_lista1_oo/src/exercicios_lista1_oo/Exercicio1.java
@@ -0,0 +1,10 @@
+package exercicios_lista1_oo;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello word");
+
+	}
+
+}
---
+++ exercicios_lista1_oo/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exercicios_lista1_oo {
+}
